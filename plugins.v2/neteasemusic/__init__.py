b3297a6cfda0f5f855a1f39575ee8899a83a7ac0
xiumuzidiao0 <107784127+xiumuzidiao0@users.noreply.github.com>
2025-09-14 23:39:10 +0800

    Update __init__.py

--- plugins.v2/neteasemusic/__init__.py
+++ plugins.v2/neteasemusic/__init__.py
@@ -74,7 +74,7 @@ class NeteaseMusic(*BaseClasses):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/xiumuzidiao0/MoviePilot-Plugins/main/icons/163music_A.png"
     # 插件版本
-    plugin_version = "1.10"
+    plugin_version = "1.28"
     # 插件作者
     plugin_author = "xiumuzidiao0"
     # 作者主页
